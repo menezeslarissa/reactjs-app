fd12de3595bd5c752dd7201fc31b9e973b09ddb0
Larissa Menezes <menezes.larissaps@gmail.com>
2020-04-16 22:54:31 -0300

    adicionando estilizações e componente de header

---
+++ huntweb/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import "./styles.css";
+
+const App = () =>(
+    <div className="App"> 
+        <Header />
+    </div>     
+);
+//componente é um conjunto de partes visuais (html) e parte funcional (js)
+// e estilização (css)
+
+export default App;
---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./styles.css";
+
+//stateless components
+//criando componente no react sem precisar criar uma classe
+//usa apenas funções
+const Header = () => <header id="main-header" > JSHunt </header>
+
+export default Header;
